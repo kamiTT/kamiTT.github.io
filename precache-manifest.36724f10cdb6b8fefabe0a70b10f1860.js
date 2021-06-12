ce1f127e8ffaab57715df1928226059d1ee7036b
Kamille Foster <kamillefoster@Kamilles-MacBook-Air.local>
2021-06-11 20:30:40 -0400

    Updates

---
+++ precache-manifest.36724f10cdb6b8fefabe0a70b10f1860.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9a9b4d33e534c7f37c0e12d10c2af66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18c1396af23223f109a2",
+    "url": "/static/css/main.7ed7da15.chunk.css"
+  },
+  {
+    "revision": "987e1f70098b5f3c3eed",
+    "url": "/static/js/2.34ab6093.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.34ab6093.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18c1396af23223f109a2",
+    "url": "/static/js/main.860cadbd.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "45f7782f0f18a54750fb9377fdb8bf0d",
+    "url": "/static/media/foster_01.45f7782f.jpg"
+  },
+  {
+    "revision": "1f456f3bb99405f053fca73567ce0e27",
+    "url": "/static/media/resume.1f456f3b.pdf"
+  }
+]);
